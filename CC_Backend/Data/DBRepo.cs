fa635940bdd5bd0473ae57a0f8bc6fe6e30c497e
ChasAlex <alexander.lundquist@chasacademy.se>
2024-04-29 10:18:23 +0200

    Added Dbrepo -For database handling

---
+++ CC_Backend/Data/DBRepo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CC_Backend.Models;
+namespace CC_Backend.Data
+{
+    public class DBRepo
+    {
+        private readonly NatureAIContext _context;
+
+        public DBRepo(NatureAIContext context)
+        {
+            _context = context;
+        }
+
+        
+        // Get all users from the database
+        public async Task<IReadOnlyList<ApplicationUser>> GetAllUsersAsync()
+        {
+            var result = await _context.Users.ToListAsync();
+            return result;
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
