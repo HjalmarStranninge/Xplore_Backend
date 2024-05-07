036e3ce19bae0ef353b6c08c08446dcce88d2fc8
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-05-07 11:38:41 +0200

    Added AccountController
    - Added /registeraccount that includes a username which the /register that came with Identity did not.
    
    - Simplified /logout a bit and moved it into AccountController instead.
    
    - Deleted LogoutController.

---
+++ CC_Backend/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using CC_Backend.Models;
+using CC_Backend.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CC_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("/registeraccount")]
+        public async Task<IActionResult> Register(RegisterDTO dto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = dto.Username, Email = dto.Email };
+                var result = await _userManager.CreateAsync(user, dto.Password);
+                if (result.Succeeded)
+                {
+                    return Ok("Registration successful");
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        /// Logs out the currently authenticated user.
+        [HttpPost("~/logout")] // This route will match "/logout"
+        public async Task<IActionResult> Logout()
+        {
+            try
+            {
+                await _signInManager.SignOutAsync();
+                return Ok("Registration successful");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error occurred during logout: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ CC_Backend/Models/DTOs/RegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CC_Backend.Models.DTOs
+{
+
+    public class RegisterDTO
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
