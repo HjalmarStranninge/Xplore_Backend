9047152e308f08d5fe8243e7e091d2d3d09b0deb
Niklas Sjödin <146171251+NiklasSjodin@users.noreply.github.com>
2024-05-08 10:12:17 +0200

    Added DTO
    - Added a DTO to the addfriend for better database management

--- CC_Backend/Repositories/FriendsRepo/IFriendsRepo.cs
+++ CC_Backend/Repositories/FriendsRepo/IFriendsRepo.cs
@@ -1,11 +1,12 @@
-﻿using CC_Backend.Models.Viewmodels;
+﻿using CC_Backend.Models.DTOs;
+using CC_Backend.Models.Viewmodels;
 
 namespace CC_Backend.Repositories.Friends
 {
     public interface IFriendsRepo
     {
         Task<IReadOnlyList<FriendViewModel>> GetFriendsAsync(string userId);
-        Task<(bool success, string message)> AddFriendAsync(string userId, string friendDisplayName);
+        Task<(bool success, string message)> AddFriendAsync(string userId, AddFriendDTO dto);
         Task<(bool success, string message)> RemoveFriendAsync(string userId, string friendDisplayName);
     }
 }
