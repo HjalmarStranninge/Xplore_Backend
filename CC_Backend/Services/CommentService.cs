005a9ab8013a7d400dda92ed4405515dca46b7d6
Niklas Sjödin <146171251+NiklasSjodin@users.noreply.github.com>
2024-05-30 15:06:33 +0200

    Made a ICommentservice and a CommentService
    - Refactored the code in the comment controller to be in a service instead for cleaner code

---
+++ CC_Backend/Services/CommentService.cs
@@ -0,0 +1,43 @@
+﻿using CC_Backend.Models;
+using CC_Backend.Models.DTOs;
+using CC_Backend.Repositories.CommentRepo;
+using CC_Backend.Repositories.StampsRepo;
+
+namespace CC_Backend.Services
+{
+    public class CommentService :ICommentService
+    {
+        private readonly IStampsRepo _stampsRepo;
+        private readonly ICommentRepo _commentRepo;
+
+        public CommentService(IStampsRepo stampsRepo, ICommentRepo commentRepo)
+        {
+            _stampsRepo = stampsRepo;
+            _commentRepo = commentRepo;
+        }
+
+        public async Task<bool> AddCommentAsync(CommentCreateDTO dto, string userID)
+        {
+            var stampCollected = await _stampsRepo.GetStampCollectedAsync(dto.StampCollectedId);
+            if(stampCollected == null)
+            {
+                throw new ArgumentException("The specified stamp collected does not exist.");
+            }
+
+            var now = DateTime.UtcNow; // Use to avoid timezone issues
+            var createdAt = new DateTime(now.Year, now.Month, now.Minute);
+
+            var comment = new Comment
+            {
+                StampCollectedId = dto.StampCollectedId,
+                Content = dto.Content,
+                StampCollected = stampCollected,
+                UserId = userID,
+                CreatedAt = createdAt
+            };
+
+            await _commentRepo.AddCommentAsync(comment);
+            return true;
+        }
+    }
+}
---
+++ CC_Backend/Services/ICommentService.cs
@@ -0,0 +1,9 @@
+﻿using CC_Backend.Models.DTOs;
+
+namespace CC_Backend.Services
+{
+    public interface ICommentService
+    {
+        Task<bool> AddCommentAsync(CommentCreateDTO dto, string userId);
+    }
+}
