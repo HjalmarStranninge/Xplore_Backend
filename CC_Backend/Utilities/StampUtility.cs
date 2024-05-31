cd5e45f4d3fd58aa32000752e273946da76ef1b2
Huan <98620169+bentonaw@users.noreply.github.com>
2024-05-31 21:14:27 +0200

    Stamps refactoring
    - Stamputility added for converting rarity double to string

---
+++ CC_Backend/Utilities/StampUtility.cs
@@ -0,0 +1,33 @@
+﻿namespace CC_Backend.Utilities
+{
+    public class StampUtility
+    {
+        public static string ConvertRarityToString(double? rarity)
+        {
+            if (!rarity.HasValue)
+            {
+                return "unknown";
+            }
+            else if (rarity.Value >= 0 && rarity.Value < 1)
+            {
+                return "Brons";
+            }
+            else if (rarity.Value >= 1 && rarity.Value < 2)
+            {
+                return "Silver";
+            }
+            else if (rarity.Value >= 2 && rarity.Value < 3)
+            {
+                return "Guld";
+            }
+            else if (rarity.Value >= 3 && rarity.Value <= 4)
+            {
+                return "Platinum";
+            }
+            else
+            {
+                return "unknown";
+            }
+        }
+    }
+}
