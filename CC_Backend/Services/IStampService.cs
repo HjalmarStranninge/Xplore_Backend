7ef16b814eacfb65f81aa7d2c96c2d6a6db9750b
Huan <98620169+bentonaw@users.noreply.github.com>
2024-06-03 17:05:01 +0200

    adding stampservice

---
+++ CC_Backend/Services/IStampService.cs
@@ -0,0 +1,10 @@
+﻿using CC_Backend.Models.Viewmodels;
+using CC_Backend.Models;
+
+namespace CC_Backend.Services
+{
+    public interface IStampService
+    {
+        List<StampViewModel> CreateStampViewModels(ICollection<StampCollected> stamps);
+    }
+}
---
+++ CC_Backend/Services/StampService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CC_Backend.Models.Viewmodels;
+using CC_Backend.Models;
+using CC_Backend.Models.DTOs;
+
+namespace CC_Backend.Services
+{
+    public class StampService : IStampService
+    {
+        public List<StampViewModel> CreateStampViewModels(ICollection<StampCollected> stamps)
+        {
+            var stampsViewModels = new List<StampViewModel>();
+
+            foreach (var stamp in stamps)
+            {
+                var stampViewModel = new StampViewModel
+                {
+                    Name = stamp.Stamp.Name,
+                    Icon = stamp.Stamp.Icon
+                };
+                stampsViewModels.Add(stampViewModel);
+            }
+            return stampsViewModels;
+        }
+
+        public StampViewModel CreateSelectedStampViewModel(int stampId)
+        {
+            var stampViewModel = new StampViewModel
+            {
+                StampId = stampId,
+                Name = stamp.Name,
+                Facts = stamp.Facts,
+                Rarity = stamp.Rarity,
+                Icon = stamp.Icon,
+                Latitude = stamp.Latitude,
+                Longitude = stamp.Longitude,
+                Category = new CategoryDTO
+                {
+                    Title = stamp.Category?.Title ?? ""
+                }
+            };
+            return stampViewModel;
+        }
+    }
+}
