09a8e6f4a9769b132cdbe12404a67155459bd16f
Huan <98620169+bentonaw@users.noreply.github.com>
2024-06-03 17:05:33 +0200

    adding userprofileviewmodel

---
+++ CC_Backend/Models/Viewmodels/UserProfileViewmodel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CC_Backend.Models.Viewmodels
+{
+    public class UserProfileViewmodel
+    {
+        public string DisplayName { get; set; }
+        public byte[]? ProfilePicture { get; set; }
+        public int StampsCollectedTotalCount { get; set; }
+        public int FriendsCount { get; set; }
+
+        public ICollection<StampViewModel>? StampCollectedTotal { get; set; }
+        public IReadOnlyList<FriendViewModel>? Friends { get; set; }
+
+    }
+}
