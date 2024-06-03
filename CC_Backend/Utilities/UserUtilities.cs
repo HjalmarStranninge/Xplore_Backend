8260209a7102bba4c8c5c3ede6d71f178da0cb97
Huan <98620169+bentonaw@users.noreply.github.com>
2024-06-03 17:03:35 +0200

    Adding userutilities
    - extractuseridfromtoken

---
+++ CC_Backend/Utilities/UserUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using CC_Backend.Models;
+
+namespace CC_Backend.Utilities
+{
+    public static class UserUtilities
+    {
+        public static string ExtractUserIdFromToken(ClaimsPrincipal user)
+        {
+            var userId = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new UnauthorizedAccessException("User ID not found in token.");
+            }
+
+            return userId;
+        }
+    }
+}
