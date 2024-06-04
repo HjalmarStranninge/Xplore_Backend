da1d56789044b2ff2380744edf45a9fe33a07aff
Huan <98620169+bentonaw@users.noreply.github.com>
2024-06-04 09:22:20 +0200

    revised stampservice

--- CC_Backend/Services/IStampService.cs
+++ CC_Backend/Services/IStampService.cs
@@ -5,6 +5,9 @@ namespace CC_Backend.Services
 {
     public interface IStampService
     {
-        List<StampViewModel> CreateStampViewModels(ICollection<StampCollected> stamps);
+        //List<StampViewModel> CreateStampViewModels(ICollection<StampCollected> stamps);
+        Task<ICollection<StampViewModel>> CreateStampViewModelsOfUser(string userId);
+        Task<SelectedStampViewModel> CreateSelectedStampViewModel(int stampId);
+        Task<CategoryWithStampListViewModel> CreateListOfCategoryStampViewModel(int categoryId);
     }
 }
--- CC_Backend/Services/StampService.cs
+++ CC_Backend/Services/StampService.cs
@@ -3,16 +3,26 @@ using System.Linq;
 using CC_Backend.Models.Viewmodels;
 using CC_Backend.Models;
 using CC_Backend.Models.DTOs;
+using CC_Backend.Repositories.StampsRepo;
+using CC_Backend.Utilities;
 
 namespace CC_Backend.Services
 {
     public class StampService : IStampService
     {
-        public List<StampViewModel> CreateStampViewModels(ICollection<StampCollected> stamps)
+        private readonly IStampsRepo _iStampRepo;
+
+        public StampService(IStampsRepo iStampRepo)
         {
+            _iStampRepo = iStampRepo;
+        }
+
+        public async Task<ICollection<StampViewModel>> CreateStampViewModelsOfUser(string userId)
+        {
+            var userStamps = await _iStampRepo.GetStampsFromUserAsync(userId);
             var stampsViewModels = new List<StampViewModel>();
 
-            foreach (var stamp in stamps)
+            foreach (var stamp in userStamps)
             {
                 var stampViewModel = new StampViewModel
                 {
@@ -24,14 +34,20 @@ namespace CC_Backend.Services
             return stampsViewModels;
         }
 
-        public StampViewModel CreateSelectedStampViewModel(int stampId)
+        public async Task<SelectedStampViewModel> CreateSelectedStampViewModel(int stampId)
         {
-            var stampViewModel = new StampViewModel
+            var stamp = await _iStampRepo.GetSelectedStampAsync(stampId);
+            if (stamp == null)
+            {
+                return null;
+            }
+
+            var selectedStampViewModel = new SelectedStampViewModel
             {
                 StampId = stampId,
                 Name = stamp.Name,
                 Facts = stamp.Facts,
-                Rarity = stamp.Rarity,
+                Rarity = StampUtility.ConvertRarityToString(stamp.Rarity),
                 Icon = stamp.Icon,
                 Latitude = stamp.Latitude,
                 Longitude = stamp.Longitude,
@@ -40,7 +56,28 @@ namespace CC_Backend.Services
                     Title = stamp.Category?.Title ?? ""
                 }
             };
-            return stampViewModel;
+            return selectedStampViewModel;
+        }
+
+        public async Task<CategoryWithStampListViewModel> CreateListOfCategoryStampViewModel(int categoryId)
+        {
+            var category = await _iStampRepo.GetStampsFromCategoryAsync(categoryId);
+            var categoryViewModel = new CategoryWithStampListViewModel
+            {
+                Title = category.Title,
+                Stamps = category.Stamps.Select(stamps => new SelectedStampViewModel
+                {
+                    StampId = stamps.StampId,
+                    Name = stamps.Name,
+                    Facts = stamps.Facts,
+                    Rarity = StampUtility.ConvertRarityToString(stamps.Rarity),
+                    Icon = stamps.Icon,
+                    Latitude = stamps.Latitude,
+                    Longitude = stamps.Longitude,
+
+                }).ToList()
+            };
+            return (categoryViewModel);
         }
     }
 }
